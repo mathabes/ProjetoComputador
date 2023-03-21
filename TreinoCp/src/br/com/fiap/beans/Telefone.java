0c281765f06003018027300f990617e2eb5ad1b4
mathcamargo <matheusoabes@gmail.com>
2023-03-21 11:58:06 -0300

    Add TreinoCp

---
+++ TreinoCp/src/br/com/fiap/beans/Telefone.java
@@ -0,0 +1,33 @@
+package br.com.fiap.beans;
+
+public class Telefone {
+	private String fixo;
+	private String celular;
+	private String comercial;
+	private String contato;
+	
+	public String getFixo() {
+		return fixo;
+	}
+	public void setFixo(String fixo) {
+		this.fixo = fixo;
+	}
+	public String getCelular() {
+		return celular;
+	}
+	public void setCelular(String celular) {
+		this.celular = celular;
+	}
+	public String getComercial() {
+		return comercial;
+	}
+	public void setComercial(String comercial) {
+		this.comercial = comercial;
+	}
+	public String getContato() {
+		return contato;
+	}
+	public void setContato(String contato) {
+		this.contato = contato;
+	}
+}
